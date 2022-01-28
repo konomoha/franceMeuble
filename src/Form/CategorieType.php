963a154a21f1c242add902192db504b38f88a8ce
konomoha <konomoha@hotmail.fr>
2022-01-28 23:36:07 +0100

    formulaire categorie

---
+++ src/Form/CategorieType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class,[
+                'required'=> false,
+                'constraints' => [
+                    new NotBlank([
+                        'message'=>"Veuillez renseigner le nom du produit."
+                    ])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
