f7269299d125423f41b2bc8a66253b663b462eab
konomoha <konomoha@hotmail.fr>
2022-02-02 01:42:43 +0100

    maj bo

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -15,10 +17,31 @@ class CategorieType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class,[
-                'required'=> false,
+                'label' => "Nom de la catégorie",
+                'attr'=>[
+                    'placeholder'=> "Saisir le nom de la catégorie"
+                ],
+                'required'=>false,
                 'constraints' => [
+                    new Length([
+                        'min' => 3, 
+                        'max' => 15,
+                        'minMessage' => "Nom trop court",
+                        'maxMessage'=> "Nom trop long"
+                    ]),
                     new NotBlank([
-                        'message'=>"Veuillez renseigner le nom du produit."
+                        'message' => "Merci de sasir un nom de catégorie"
+                    ])
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr'=> [
+                    'placeholder' => "Saisir la description de la catégorie",
+                ],
+                'required'=>false,
+                'constraints'=>[
+                    new NotBlank([
+                        'message'=>"Merci de saisir une description"
                     ])
                 ]
             ])
