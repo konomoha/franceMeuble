6f6661ad05a4408414b3ba27af02d4e5847e84a1
konomoha <konomoha@hotmail.fr>
2022-02-10 01:26:10 +0100

    maj template index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Repository\AssortimentRepository;
 use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +13,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(CategorieRepository $repoCategory, EntityManagerInterface $manager): Response
+    public function index(CategorieRepository $repoCategory, AssortimentRepository $repoAssortiment, EntityManagerInterface $manager): Response
     {
         
         $colonnes = $manager->getClassMetadata(Categorie::class)->getFieldNames();
 
         $category = $repoCategory->findAll();
 
+        $assortiment = $repoAssortiment->findNewest();
+
         return $this->render('boutique/index.html.twig', [
             'controller_name' => 'HomeController',
             'colonnes'=>$colonnes,
-            'category'=>$category
+            'category'=>$category,
+            'assortiment'=>$assortiment
         ]);
     }
 
--- src/Form/AssortimentType.php
+++ src/Form/AssortimentType.php
@@ -1,16 +1,14 @@
 <?php
 
 namespace App\Form;
-
-use App\Entity\Produit;
-use App\Form\ProductType;
 use App\Entity\Assortiment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AssortimentType extends AbstractType
@@ -36,6 +34,28 @@ class AssortimentType extends AbstractType
                     ])
                 ]
             ])
+
+            ->add('photo', FileType::class, [
+                'label' => "Uploader une photo",
+                'mapped' => true, 
+                'data_class'=> null,
+                'required'=> false,
+                
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                            'image/gif'
+                        ],
+                        'mimeTypesMessage' => 'Formats autorisés : jpg/jpeg/png',
+                        'maxSizeMessage'=> 'Taille maximale: 5mo'
+                    ])
+                ]
+            ])
+
             
         ;
     }
