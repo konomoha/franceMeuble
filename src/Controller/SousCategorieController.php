4eb01f76e09205035c8340680fc6832ba878b3a1
konomoha <konomoha@hotmail.fr>
2022-03-28 02:14:40 +0200

    maj template

--- src/Controller/SousCategorieController.php
+++ src/Controller/SousCategorieController.php
@@ -12,9 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SousCategorieController extends AbstractController
 {
+    // ############################################ LISTE DES SOUS-CATEGORIES ################################################
+
     #[Route('/subcategory', name: 'boutique_subcategories')]
     public function allSubCategories(SouscategorieRepository $subcatRepo, EntityManagerInterface $manager): Response
     {
+        // Nous récupérons ici toutes les informations des catégories présentes en bdd. Elles sont ensuite transmises au template sous_categories_list afin de le réutiliser dans les menus de navigation.
+
         $colonnes = $manager->getClassMetadata(Souscategorie::class)->getFieldNames();
 
         $subcategories = $subcatRepo->findAll();
@@ -25,15 +29,19 @@ class SousCategorieController extends AbstractController
         ]);
     }
 
+    // ############################################ AFFICHAGE D'UNE SOUS-CATEGORIE ############################################
+
     #[Route('/subcategory/{id}', name: 'boutique_subcategory_show')]
     public function subCategoryShow(Souscategorie $souscategorie, SouscategorieRepository $subcatRepo, ProduitRepository $productRepo)
     {
-        $subcatData = $subcatRepo->find($souscategorie);
-        $id = $souscategorie->getId();
-        $products = $productRepo->findGroup($id);
-        
-        
+        // On récupère ici l'id de la sous-catégorie sélectionnée. l'array subcatData contient toutes les informations de la sous-catégorie. On se sert également de la méthode construite dans ProduitRepository : findGroup(). Cette méthode attend un id en argument et permet de trouver tous les produits appartenant à une sous-catégorie précise. Elle regroupe ensuite tous les produits trouvés par nom et retourne le résultat sous la forme d'un array.
 
+        $subcatData = $subcatRepo->find($souscategorie);
+
+        $id = $souscategorie->getId();
+        
+        $products = $productRepo->findGroup($id);
+    
         return $this->render('sous_categorie/sous_categorie.html.twig', [
             'subcatData' => $subcatData,
             'products' => $products
